be17cb28d3e1f33d6362822d8e1cfa8eb0aef32d
Aliwb6 <alirezakarimi.d.n@gmail.com>
2026-05-30 02:57:28 +0330

    First commit

---
+++ src/pages/Invite.jsx
@@ -0,0 +1,11 @@
+export default function Invite() {
+  return (
+    <div style={{ width: '100%', height: '100dvh', overflow: 'hidden' }}>
+      <iframe
+        src="/index.html"
+        style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
+        title="دعوت‌نامه"
+      />
+    </div>
+  );
+}
