f90fd4ca2404c778b98c5f27d86909bebb6e343f
Aliwb6 <alirezakarimi.d.n@gmail.com>
2026-05-30 03:52:42 +0330

    edit again  vrcel.json file last

--- src/pages/Invite.jsx
+++ src/pages/Invite.jsx
@@ -2,7 +2,7 @@ export default function Invite() {
   return (
     <div style={{ width: '100%', height: '100dvh', overflow: 'hidden' }}>
       <iframe
-        src="/index.html"
+        src="/invite.html"
         style={{ width: '100%', height: '100%', border: 'none', display: 'block' }}
         title="دعوت‌نامه"
       />
